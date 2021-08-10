83e2556d3a0ea2c6e4db446ad00d1aae722665f3
Maheshbabu78 <77149821+Maheshbabu78@users.noreply.github.com>
2021-08-10 15:31:25 +0530

    testcases

--- demo_test.go
+++ demo_test.go
@@ -7,8 +7,6 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/assert/v2"
-	"main.go/productCrud/controllers"
 	"main.go/productCrud/models"
 	"main.go/productCrud/routes"
 )
@@ -66,27 +64,12 @@ func TestProductStruct(t *testing.T) {
 	}
 
 }
-func TestGetProduct(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	handler := controllers.Findproductbyid
-	router := gin.Default()
-	router.GET("/product/:id", handler)
-
-	req, err := http.NewRequest("GET", "/product/2", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	resp := httptest.NewRecorder()
-	router.ServeHTTP(resp, req)
-	//assert.Equal(t, resp.Code, 200)
-	assert.Equal(t, resp.Code, 200)
-}
 func TestGetProducts(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	db := models.ConnecttoDb()
 	db.AutoMigrate(&models.Product{})
 	testRouter := routes.RoutersNavigation(db)
-	req, err := http.NewRequest(http.MethodGet, "/product/8", nil)
+	req, err := http.NewRequest(http.MethodGet, "/product/9", nil)
 	if err != nil {
 		t.Fatalf("Couldn't create request: %v\n", err)
 	}
